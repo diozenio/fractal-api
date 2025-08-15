7cd88cb68b266e688908856328f3de4ff658cc1d
diozenio <d.dionisiobsl@gmail.com>
2025-08-15 13:19:13 -0300

    refactor: Update default port from 3333 to 8080

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
